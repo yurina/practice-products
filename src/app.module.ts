c6a9a26eb2995eb49dee8eb992475c667d59c1c7
Irina Yurina <irina.yurina@gmail.com>
2026-01-15 21:25:02 +0100

    chore: add typeorm, docker postgres, products resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
